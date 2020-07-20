71e8e2cde2145f5ea1b7f40b4cd19f66dd5f35a6
zhangke-cs <zk2973_p@163.com>
2020-07-20 11:17:28 +0800

    init test data

---
+++ data/test_data/create_test_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Create Test Config File
+#Usage:
+#bash $0 > test.configfile
+
+CONFIG_DATA="cat"
+"${CONFIG_DATA}" <<EOF
+SampleName = test_sample
+WorkFolder = test_sample
+LibraryInfo = ${FASASHome}/data/library_info/126_length_library_info.txt
+LinkedLibraryR1 = ${FASASHome}/data/raw_data/20200509_16S_63_R1.fq.gz
+LinkedLibraryR2 = ${FASASHome}/data/raw_data/20200509_16S_63_R2.fq.gz
+ReadLibraryR1 = ${FASASHome}/data/raw_data/20200509_16S_26_R1.fq.gz
+ReadLibraryR2 = ${FASASHome}/data/raw_data/20200509_16S_26_R2.fq.gz
+CoverageDatabase = ${FASASHome}/data/database/mini_fulllength.fasta
+ReferenceFasta = ${FASASHome}/data/database/mini_fulllength.fasta
+ReferenceTaxonomy = ${FASASHome}/data/datbase/mini_taxonomy.txt
+AssembleProgram = idba_ud
+IlluminaAdapter = CTGTCTCTTATACACATCT
+ContigLength = 1200
+EOF
---
+++ data/test_data/database/build_database.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+if [ $(which bowtie2-build 2>/dev/null) ] ; then
+    echo "Found bowtie2-build command."
+else
+    echo "bowtie2-build command not found!"
+    echo "exit."
+    exit
+fi
+
+if [ $(which makeblastdb 2>/dev/null)] ; then
+    echo "Found makeblastdb command."
+else
+    echo "makeblastdb command not found!"
+    echo "exit"
+    exit
+fi
+
+if [ $(which makembindex 2>/dev/null)] ; then
+    echo "Found makembindex command."
+else
+    echo "makembindex command not found!"
+    echo "exit"
+    exit
+fi
+
+if [ $1 -eq '' ] ; then
+    echo "Requires a parameter, INPUT_FILE."
+    echo "exit."
+    exit
+fi
+
+test -e $1 || { echo "INPUT_FILE: ${INPUT_FILE} is not found!"; echo "exit."; exit; }
+
+INPUT_FILE=$1
+INPUT_FOLDER=$(dirname ${INPUT_FILE})
+INPUT_BASENAME=$(basename ${INPUT_FILE})
+cd ${INPUT_FOLDER}
+
+echo "Build bowtie2 index."
+bowtie2-build ${INPUT_BASENAME} ${INPUT_BASENAME} >/dev/null
+echo "Build blastn index."
+makeblastdb -in ${INPUT_BASENAME} -input_type fasta -dbtype nucl
+makembindex -iformat blastdb -input ${INPUT_BASENAME}
+
+echo "Done."
